eced5e29d9154bb42c9f357f9296f3ae84c96350
Gagan7348 <tulsimahima111@gmail.com>
2026-06-03 02:11:18 +0530

    Update birth-want-to-say to English with advanced styling

--- docs/birth-want-to-say/config.js
+++ docs/birth-want-to-say/config.js
@@ -1,65 +1,32 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
-    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
-    // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的姑娘",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日！",
-        "这两年来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
-        "今年要吃好好的",
-        "喂饱饱的",
-        "然后才有力气干活",
-        "和想我！",
-        "生日快乐~~",
+        "For Avani",
+        "Happy Birthday",
+        "Happy Birthday Avani",
+        "Today is all about you",
+        "This little surprise was made with care",
+        "You are truly special",
+        "You make ordinary moments brighter",
+        "You bring warmth wherever you go",
+        "You deserve a day full of smiles",
+        "You deserve love that feels effortless",
+        "May this year bring peace to your heart",
+        "May your dreams grow beautifully",
+        "May your smile stay as bright as today",
+        "We all wish you happiness",
+        "We all wish you love",
+        "Happy Birthday Avani"
     ],
-    /**
-     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
-     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
-     * 例如
-     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
-     *
-     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
-     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
-     */
-    imgs: {
-        "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
-        "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
-        "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
-    },
-    
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    imgs: {},
     desc: {
-        turn_on: "点我开始",
-        play: "上音乐",
-        bannar_coming: "搞点颜色",
-        balloons_flying: "好像还少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐～",
-        story: "A MESSAGE FOR YOU"
+        turn_on: "Begin The Surprise",
+        play: "Play Music",
+        bannar_coming: "Light Up The Room",
+        balloons_flying: "Release The Balloons",
+        cake_fadein: "Bring The Cake",
+        light_candle: "Light The Candle",
+        wish_message: "Happy Birthday Avani",
+        story: "A MESSAGE FOR AVANI"
     },
-
-    // 结束语
-    loveText: '爱你的X先生~'
+    loveText: 'With love, from me'
 };
