d1cf3f02694e8cbee89adeb5405590a40883352f
Gagan7348 <tulsimahima111@gmail.com>
2026-06-03 01:23:05 +0530

    Deploy full project with home page, chat, and birthday cake to GitHub Pages

---
+++ docs/chat-want-to-say/birthday-cake/config.js
@@ -0,0 +1,32 @@
+var config = {
+    texts: [
+        "For Avani",
+        "Happy Birthday",
+        "Happy Birthday Avani",
+        "Today is all about you",
+        "This little surprise was made with care",
+        "You are truly special",
+        "You make ordinary moments brighter",
+        "You bring warmth wherever you go",
+        "You deserve a day full of smiles",
+        "You deserve love that feels effortless",
+        "May this year bring peace to your heart",
+        "May your dreams grow beautifully",
+        "May your smile stay as bright as today",
+        "We all wish you happiness",
+        "We all wish you love",
+        "Happy Birthday Avani"
+    ],
+    imgs: {},
+    desc: {
+        turn_on: "Begin The Surprise",
+        play: "Play Music",
+        bannar_coming: "Light Up The Room",
+        balloons_flying: "Release The Balloons",
+        cake_fadein: "Bring The Cake",
+        light_candle: "Light The Candle",
+        wish_message: "Happy Birthday Avani",
+        story: "A MESSAGE FOR AVANI"
+    },
+    loveText: 'With love, from me'
+};
